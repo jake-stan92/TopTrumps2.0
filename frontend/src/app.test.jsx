e0f039871c72c3e0f1de751eda3ecf13e9595d1c
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-07-23 12:48:51 +0100

    failing tests removed

--- frontend/src/app.test.jsx
+++ frontend/src/app.test.jsx
@@ -5,6 +5,6 @@ import App from "./App.jsx";
 describe("App", () => {
   test("renders", () => {
     render(<App />);
-    expect(screen.getByText("Football !!Top Trumps")).toBeDefined();
+    expect(screen.getByText("Football Top Trumps")).toBeDefined();
   });
 });
